00a8abe8149f5a927fc6bd274f4c85417a1b7c3c
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-10-07 19:17:55 +1200

    tests

--- verification/tests.py
+++ verification/tests.py
@@ -33,9 +33,31 @@ TESTS = {
     ],
     "Extra": [
         {
-            "input": "Dog,cat,mouse,bird.Human.",
-            "answer": 3,
-            "explanation": 0
-        },
+            "input": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+            "answer": 5},
+        {
+            "input": "To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it?",
+            "answer": 8},
+        {
+            "input": "The European languages are members of the same family. Their separate existence is a myth.",
+            "answer": 6},
+        {
+            "input": "For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words.",
+            "answer": 6},
+
+    ],
+    "Extra2": [
+        {
+            "input": "1 2 3 12 13",
+            "answer": 0},
+        {
+            "input": "1st 2a ab3er root rate",
+            "answer": 1},
+        {
+            "input": "JJJ:qwerty,iddqd,hoho",
+            "answer": 1},
+        {
+            "input": "I can see dead people. yes,no.",
+            "answer": 2},
     ]
 }
