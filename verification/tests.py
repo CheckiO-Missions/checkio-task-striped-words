462192e574752772fa8bd59573cdb6167c2b8e4e
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-10-07 17:31:42 +1200

    base tests and fixes

--- verification/tests.py
+++ verification/tests.py
@@ -7,30 +7,35 @@ Each test is dict with
     "explanation" -- not necessary key, it's using for additional info in animation.
 """
 
-
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": "My name is ...",
+            "answer": 3,
+            "explanation": 0
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
+            "input": "Hello world",
+            "answer": 0,
+            "explanation": 0
+        },
+        {
+            "input": "A quantity of striped words.",
+            "answer": 1,
+            "explanation": 0
+        },
+        {
+            "input": "Dog,cat,mouse,bird.Human.",
+            "answer": 3,
+            "explanation": 0
+        },
+
     ],
     "Extra": [
         {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
+            "input": "Dog,cat,mouse,bird.Human.",
+            "answer": 3,
+            "explanation": 0
         },
-        {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
     ]
 }
